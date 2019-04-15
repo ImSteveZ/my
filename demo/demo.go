229690755f61b6f22f6e165e5b09ac9364c9e956
Steve Zhang <stevzhang01@gmail.com>
2019-04-15 10:05:36 +0800

    rename some type and functions

--- demo/demo.go
+++ demo/demo.go
@@ -7,25 +7,25 @@ import (
 )
 
 func main() {
-	myMux := mygo.NewMyMux()
-	myMux.ServeFile("/", "./public")
-	myMux.HandleFunc("/index/", auth, index)
-	myMux.HandleFunc("/usr/", auth, usr)
-	log.Fatal(http.ListenAndServe(":3000", myMux))
+	myx := mygo.NewMyx()
+	myx.ServeFile("/", "./public")
+	myx.HandleFunc("/index/", Auth, Index)
+	myx.HandleFunc("/usr/", Auth, Usr)
+	log.Fatal(http.ListenAndServe(":3000", myx))
 }
 
-func auth(ctx *mygo.Ctx, w http.ResponseWriter, r *http.Request) bool {
+func Auth(ctx *mygo.Ctx, w http.ResponseWriter, r *http.Request) bool {
 	ctx.Set("auth", "login")
 	return true
 }
 
-func index(ctx *mygo.Ctx, w http.ResponseWriter, r *http.Request) bool {
+func Index(ctx *mygo.Ctx, w http.ResponseWriter, r *http.Request) bool {
 	authval, _ := ctx.Get("auth")
 	w.Write([]byte("index_" + authval.(string)))
 	return true
 }
 
-func usr(ctx *mygo.Ctx, w http.ResponseWriter, r *http.Request) bool {
+func Usr(ctx *mygo.Ctx, w http.ResponseWriter, r *http.Request) bool {
 	authval, _ := ctx.Get("auth")
 	w.Write([]byte("usr_" + authval.(string)))
 	return true
--- myx.go
+++ myx.go
@@ -5,9 +5,9 @@ import (
 	"sync"
 )
 
-// Mux类型包装了一个原生的http.ServerMux类型的指针
+// Myx类型包装了一个原生的http.ServerMux类型的指针
 // 并提供自有的HandleFunc方法
-type Mux struct {
+type Myx struct {
 	once     sync.Once
 	ServeMux *http.ServeMux
 }
@@ -16,9 +16,9 @@ type Mux struct {
 // 后续方法集将不再执行。未初始化ServeMux成员的Mux在调用HandleFunc方法时会自动进行一次初始化操作，
 // sync.Once保证了该初始化操作的并发安全和单次执行, 在使用过程中应避免将ServeMux赋值为nil，因为这会导致空
 // 指针操作的panic
-func (mux *Mux) HandleFunc(pattern string, funcs ...func(*Ctx, http.ResponseWriter, *http.Request) bool) {
-	mux.once.Do(mux.initServeMux) // 检测初始化ServeMux
-	mux.ServeMux.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+func (myx *Myx) HandleFunc(pattern string, funcs ...func(*Ctx, http.ResponseWriter, *http.Request) bool) {
+	myx.once.Do(myx.initServeMux) // 检测初始化ServeMux
+	myx.ServeMux.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
 		ctx := new(Ctx)
 		for _, f := range funcs {
 			if !f(ctx, w, r) {
@@ -30,22 +30,22 @@ func (mux *Mux) HandleFunc(pattern string, funcs ...func(*Ctx, http.ResponseWrit
 }
 
 // ServeFile方法开启一个文件服务
-func (mux *Mux) ServeFile(pattern, dir string) {
+func (myx *Myx) ServeFile(pattern, dir string) {
 	fileHandle := http.FileServer(http.Dir(dir))
-	mux.ServeMux.Handle(pattern, http.StripPrefix(pattern, fileHandle))
+	myx.ServeMux.Handle(pattern, http.StripPrefix(pattern, fileHandle))
 }
 
-// Mux类型实现http.Handler接口
-func (mux *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	mux.ServeMux.ServeHTTP(w, r)
+// ServeHTTP方法实现http.Handler接口
+func (myx *Myx) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	myx.ServeMux.ServeHTTP(w, r)
 }
 
 // initServeMux初始化Mux.ServeMux成员
-func (mux *Mux) initServeMux() {
-	if mux.ServeMux == nil {
-		mux.ServeMux = http.NewServeMux()
+func (myx *Myx) initServeMux() {
+	if myx.ServeMux == nil {
+		myx.ServeMux = http.NewServeMux()
 	}
 }
 
-// NewMyMux方法返回一个初始化了ServeMux的Mux类型指针
-func NewMyMux() *Mux { return &Mux{ServeMux: http.NewServeMux()} }
+// NewMyx方法返回一个初始化了ServeMux的Myx类型指针
+func NewMyx() *Myx { return &Myx{ServeMux: http.NewServeMux()} }
